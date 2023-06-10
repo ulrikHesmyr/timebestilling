94f052c9ae9beaa2880f27cf82ec794b649e864b
ulrikHesmyr <ulrik.hesmyr2002@gmail.com>
2023-06-10 10:52:04 +0200

    g

--- client/src/components/Tjenester.js
+++ client/src/components/Tjenester.js
@@ -1,4 +1,4 @@
-import React, {useState, useRef} from "react";
+import React, {useState, useRef, useEffect} from "react";
 import Fortsett from "./Fortsett.js";
 import { Link } from "react-router-dom";
 
